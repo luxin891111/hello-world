66a3c41b5aa135ed797a716e18ac70ee49eb5109
luxin <luxin7@huawei.com>
2016-12-28 11:48:48 +0800

    添加log4j的配置文件和jar包

--- src/com/github/helloworld/HelloWorld.java
+++ src/com/github/helloworld/HelloWorld.java
@@ -1,10 +1,15 @@
 
 package com.github.helloworld;
 
+import org.apache.log4j.Logger;
+
 public class HelloWorld {
+
+    private static Logger logger = Logger.getLogger(HelloWorld.class);
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        logger.info("Success!");
 
     }
 }
